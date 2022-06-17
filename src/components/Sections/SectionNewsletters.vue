d6ebce97fc5fb835401c0a34e50a39d7e0177385
Levva01 <vellanifederico13@gmail.com>
2022-06-17 15:54:01 +0200

    Newsletter section completed

--- src/components/Sections/SectionNewsletters.vue
+++ src/components/Sections/SectionNewsletters.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="newsletter">
 
-    <h2><strong>Subscribe</strong>Newsletters</h2>
+    <h2><strong>Subscribe </strong>Newsletters</h2>
 
     <p>Enter your email address to register our newsletter subscription delivered on a regular basis!</p>
 
@@ -22,5 +22,55 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.newsletter{
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 5rem 0;
+
+  .email-bar{
+    width: 30%;
+    height: 4.375rem;
+    display: flex;
+
+    input{
+      width: 70%;
+      background-color: #f8f8f8;
+      border: none;
+    }
+
+    input::placeholder{
+      padding-left: .625rem;
+    }
+
+    button{
+      width: 30%;
+      color: white;
+      background-color: #20ad96;
+      border: none;
+      border-top-right-radius: .3125rem;
+      border-bottom-right-radius: .3125rem;
+    }
+
+
+  }
+
+
+  h2{
+    font-size: 1.875rem;
+    strong{
+      color: #20ad96;
+    }
+  }
+
+  p{
+    color: gray;
+    width: 30%;
+    text-align: center;
+    margin-bottom: 1.875rem;
+  }
+}
 
 </style>
